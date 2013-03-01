8c5771f3ba91e8c63eaa8ed602a31b6b511275a8
Luis Bacelar <lmbacelar@gmail.com>
2013-03-01 15:12:11 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cassette
+
+## Description
+This app implements a logger for multiple simultaneous measuring equipment. Goals are:
+
+* Multi-user
+* Multi-instrument
+* Multi-channel
+* Instrument independent
+* Graphical and numerical analysis
+
+
+## Install
+Just clone
+
+    `git clone https://github.com/lmbacelar/cassette.git`
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cassette::Application.config.secret_token = '335d33b10d72b1c937fd5f9005570b613b14d0fd0c9b46cf463c5f57c61480258a6181286aeafc74a5629b73ea833571940be469fa830091654d4532fd1f8566'
