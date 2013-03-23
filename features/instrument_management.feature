8b8241087f49b66c1b3b9280ee77d5ce1a9cbc5b
Luis Bacelar <lmbacelar@gmail.com>
2013-03-23 12:06:21 +0000

    Keyword bracketing. Step definition refactoring.

--- features/instrument_management.feature
+++ features/instrument_management.feature
@@ -1,19 +1,19 @@
 Feature: A metrologist can manage instruments
 
   Scenario: Instruments list
-    Given I have the following instrument records
+    Given I have the following "instrument" records
       | name   | description                     |
       | sprt01 | Standard Platinum Resistance 01 |
       | rtd01  | Resistance Thermal Detector 01  |
-    When I list instruments
+    When I list "instruments"
     Then I should see "sprt01"
     And I should see "Standard Platinum Resistance 01"
     And I should see "rtd01"
     And I should see "Resistance Thermal Detector 01"
 
   Scenario: Create valid instrument
-    Given I have no instruments
-    And I list instruments
+    Given I have no "instruments"
+    And I list "instruments"
     When I follow "New Instrument"
     And I fill in "Name" with "sprt01"
     And I fill in "Description" with "Standard Platinum Resistance 01"
@@ -21,24 +21,24 @@ Feature: A metrologist can manage instruments
     Then I should see "Instrument was successfully created."
     And I should see "sprt01"
     And I should see "Standard Platinum Resistance 01"
-    And I should have 1 instrument
+    And I should have 1 "instrument"
 
   Scenario: Edit an instrument
-    Given I have only one instrument named sprt01
-    And I list instruments
+    Given I have only one "instrument" named "sprt01"
+    And I list "instruments"
     When I follow "sprt01"
     And I follow "Edit"
     And I fill in "Name" with "sprt02"
     And I press "Update"
     Then I should see "Instrument was successfully updated."
     And I should see "sprt02"
-    And I should have 1 instrument
+    And I should have 1 "instrument"
 
   Scenario: Destroy an instrument
-    Given I have only one instrument named sprt01
-    And I list instruments
+    Given I have only one "instrument" named "sprt01"
+    And I list "instruments"
     When I follow "sprt01"
     And I follow "Destroy"
     Then I should see "Instrument was successfully destroyed."
     And I should not see "sprt01"
-    And I should have 0 instruments
+    And I should have 0 "instruments"
--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,27 +1,40 @@
-Given /^I have the following (.*) records$/ do |factory, table|
+Given /^I have the following "(.*)" records$/ do |factory, table|
   table.hashes.each do |hash|
     FactoryGirl.create factory, hash
   end
 end
 
-Given /^I have one (.*) named (.*)$/ do |factory, name|
+Given /^I have the following "(.*?)" records for "(.*?)" named "(.*?)"$/ do |child, parent, parent_name, table|
+  parent_klass = parent.capitalize.constantize
+  table.hashes.each do |hash|
+    hash[parent.to_sym] = parent_klass.find_by_name(parent_name)
+    FactoryGirl.create child, hash
+  end
+end
+
+Given /^I have one "(.*)" named "(.*)"$/ do |factory, name|
   FactoryGirl.create factory, name: name
 end
 
-Given /^I have no (.*)/ do |model|
+Given /^I have no "(.*)"/ do |model|
   model.singularize.capitalize.constantize.delete_all
 end
 
-Given /^I have only one (.*) named (.*)$/ do |model, name|
-  step "I have no #{model}"
-  step "I have one #{model} named #{name}"
+Given /^I have only one "(.*)" named "(.*)"$/ do |model, name|
+  step "I have no \"#{model}\""
+  step "I have one \"#{model}\" named \"#{name}\""
 end
 
-Then /^I should have (\d+) (.*)s?$/ do |count, model|
+Then /^I should have (\d+) "(.*)"$/ do |count, model|
   expect(model.singularize.capitalize.constantize.count).to eq count.to_i
 end
-When /^I list (.*)$/ do |model|
+When /^I list "([^\"]*)"$/ do |model|
   visit eval("#{model}_path")
+end
+
+When /^I list "([^\"]*)" for "([^\"]*)" named "([^\"]*)"$/ do |children, parent, parent_name|
+  parent_klass = parent.capitalize.constantize
+  visit eval("#{parent}_#{children}_path(#{parent_klass.find_by_name(parent_name).id})")
 end
 
 Then /^I should see "(.*?)"$/ do |keyword|
@@ -36,9 +49,9 @@ When /^I follow "(.*?)"$/ do |link|
   click_link link
 end
 
-When /^I follow the "([^\"]*)" link for "([^\"]*)"$/ do |link, cell_value|
-  within "//*[.//text()='#{cell_value}' and .//a[text()='#{link}']]" do |scope|
-    scope.click_link link
+When /^I follow "([^\"]*)" on the "([^\"]*)" element$/ do |link, element|
+  within(".#{element}") do
+    click_link link
   end
 end
 
@@ -50,3 +63,6 @@ When /^I press "(.*?)"$/ do |button|
   click_button button
 end
 
+Given /^I am on the page of "([^\"]*)" named "([^\"]*)"$/ do |model, name|
+  visit eval("#{model}_path(model.capitalize.constantize.find_by_name(name).id)")
+end
